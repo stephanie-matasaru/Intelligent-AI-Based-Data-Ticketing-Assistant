8d5d2ef9b7df848b6d6903d1e0fdfb422f001f86
predutalexia <alexia.predut05@e-uvt.ro>
2026-05-04 17:15:53 +0300

    stored export file path in db

--- backend/services/chat_service.py
+++ backend/services/chat_service.py
@@ -1,14 +1,22 @@
 from db import get_connection
+from routes.chat_history import generate_title
 import uuid
 
-def save_message(user_id, sender, message, query=None, tokens=None, status="pending", group_id=None):
+def save_message(user_id, sender, message, query=None, tokens=None, status="pending", group_id=None, export_file_path=None):
     try:
         conn = get_connection()
         cursor = conn.cursor()
+
+        title = None
+        if sender == "user":
+            cursor.execute("SELECT COUNT(*) FROM chat_messages WHERE group_id = ?", (str(group_id),))
+            count = cursor.fetchone()[0]
+            if count == 0:
+                title = generate_title(message)
         cursor.execute("""
             INSERT INTO chat_messages 
-                (group_id, user_id, sender, message, query, request_tokens, response_status)
-            VALUES (?, ?, ?, ?, ?, ?, ?)
+                (group_id, user_id, sender, message, query, request_tokens, response_status, title, export_file_path)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
         """, (
             str(group_id) if group_id else str(uuid.uuid4()),
             user_id,
@@ -16,7 +24,9 @@ def save_message(user_id, sender, message, query=None, tokens=None, status="pend
             message,
             query,
             tokens,
-            status
+            status,
+            title,
+            export_file_path
         ))
         conn.commit()
         conn.close()
@@ -28,7 +38,7 @@ def get_chat_history_by_user(user_id: int):
     conn = get_connection()
     cursor = conn.cursor()
     cursor.execute("""
-        SELECT group_id, sender, message, query, response_status, date_added
+        SELECT group_id, sender, message, query, response_status, date_added, export_file_path
         FROM chat_messages
         WHERE user_id = ? AND delete_flag = 0
         ORDER BY date_added ASC
